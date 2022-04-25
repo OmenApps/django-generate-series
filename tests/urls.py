d3e23690ce2db2c29c6b3d1ebc6e2cbbdd2fac2f
Jack Linke <jacklinke@gmail.com>
2022-04-24 22:29:27 -0400

    Remove another unneeded includes

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
-    path("", include("django_generate_series.urls", namespace="django_generate_series")),
 ]
