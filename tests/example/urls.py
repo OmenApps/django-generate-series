0a49f6f3191cdf0e45efcbd49a9e30237a1ea07f
Jack Linke <jacklinke@gmail.com>
2022-04-24 22:25:27 -0400

    Correct include of obsolete urls file

--- tests/example/urls.py
+++ tests/example/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("django_generate_series.urls", namespace="django_generate_series")),
 ]
