0db2b2cb75a77264916a0b369d9c43cb6fef661b
Jack Linke <jacklinke@gmail.com>
2026-02-13 00:13:14 -0500

    Fix for warnings about db access before init

--- tests/example/core/apps.py
+++ tests/example/core/apps.py
@@ -1,15 +1,13 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-from django.db import DEFAULT_DB_ALIAS, connections
-from django.db.migrations.executor import MigrationExecutor
+from django.db.models.signals import post_migrate
 
 
-def is_database_synchronized(database):
-    connection = connections[database]
-    connection.prepare_database()
-    executor = MigrationExecutor(connection)
-    targets = executor.loader.graph.leaf_nodes()
-    return not executor.migration_plan(targets)
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if not User.objects.filter(username="admin", email="admin@example.com").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "pass")
 
 
 class CoreConfig(AppConfig):
@@ -17,8 +15,4 @@ class CoreConfig(AppConfig):
     name = "tests.example.core"
 
     def ready(self) -> None:
-        if is_database_synchronized(DEFAULT_DB_ALIAS):
-            User = get_user_model()
-            user_exists = User.objects.filter(username="admin", email="admin@example.com").exists()
-            if not user_exists:
-                User.objects.create_superuser("admin", "admin@example.com", "pass")
+        post_migrate.connect(create_superuser, sender=self)
